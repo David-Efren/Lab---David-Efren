877672c3eef7016a96b4607ce9261e010be9610a
David-Efren <terronesirl@gmail.com>
2025-09-03 22:17:54 -0600

    Fixed

--- Programas/p032_PagoEstacionamiento.java
+++ Programas/p032_PagoEstacionamiento.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class p032_PagoEstacionamiento {
 	public static void main(String[] args) {
 		Scanner obj = new Scanner(System.in);
-		float tasa=0, paga, impuesto=0, total;
+		float tasa=0, paga, impuesto, total;
 		int est;
 		
 		System.out.println("ELIGE\n");
@@ -14,12 +14,15 @@ public class p032_PagoEstacionamiento {
 		System.out.print("\n\nEstacionamiento?  "); est = obj.nextInt();
 		System.out.print("\n\nPaga?  "); paga = obj.nextFloat();
 		
-		switch (n) {
+		switch (est) {
 			case 1 : tasa = 3f; break;
 			case 2 : tasa = 5f; break;
 			case 3 : tasa = 10f; break;
 			case 4 : tasa = 20f; break;
-			default :
-				System.out.println("\n\nNo")
+			default : System.out.println("\n\nNo"); break;
 	}
+	impuesto = (paga * tasa);
+	total = paga + impuesto;
+	System.out.printf("\nElegiste el estacionamiento %d \nPagaste %.2f por el tiempo de uso \nCorresponde un impuesto de %,.2f \nEl pago total es de %,.2f",est,paga,impuesto,total);
+}
 }
